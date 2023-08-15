805f5fd331102528f05cd10c801b2c094bbdf02f
Tush786 <tusharsapate34@gmail.com>
2023-08-15 14:42:10 +0530

    Add search functionality

--- Productpage/my-app/src/Component/Productpage.jsx
+++ Productpage/my-app/src/Component/Productpage.jsx
@@ -10,10 +10,48 @@ function Productpage() {
     const [totalPage, setTotalPage] = useState([])
     const [sortby, setSortby] = useState('')
     const[all,setAll]=useState("")
+    const [search,setSearch]=useState('')
+
+
     let limit = 9;
     let Length = totalPage.length;
     console.log(Length)
 
+
+    // <------------------- Search functionality ------------------>
+
+    const SearchValue=Debouncing(search,1000)
+
+  function Debouncing(search, delay) {
+
+    const [debouncedValue, setDebouncedValue] = useState(search);
+
+    useEffect(() => {
+      const timer = setTimeout(() => {
+        setDebouncedValue(search);
+      }, delay);
+
+      return () => {
+        clearTimeout(timer);
+      };
+    }, [search, delay]);
+    return debouncedValue;
+  }
+
+console.log(SearchValue);
+const FetchData_Search=async(SearchValue)=>{
+  try {
+    let Resp=await fetch(`http://localhost:8080/Product?q=${SearchValue}`)
+    let Data=await Resp.json();
+    setArr(Data);
+    console.log(Data)
+  } catch (error) {
+    console.log(error)
+  }
+}
+// <-------------- End Search Functionality ---------------->
+
+// <=================== Display Data -------------------->
     function getUrl(url,sortby) {
         if (sortby) {
             return `${url}&_sort=cost&_order=${sortby}`
@@ -49,12 +87,20 @@ function Productpage() {
 
 
     useEffect(() => {
+       if(SearchValue!==""){
+        FetchData_Search(SearchValue)
+       }
+       else{
         FetchData()
         TotalPage()
-    }, [page,sortby])
+       }
+        
+    }, [page,sortby,SearchValue])
 
     return (
         <div>
+          {/* <input data-testid="search_key" type="search" placeholder="Search Movies" value={search} onInput={(e)=>setSearch(e.target.value)}></input> */}
+
             <div className={style.filterbtn}>
                 <button onClick={()=>setAll("")}>All</button>
                 <button>Trending</button>
